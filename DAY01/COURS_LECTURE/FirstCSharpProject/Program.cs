09d0945dc2f5c5d7714775f617365936bf188b90
Yaya Dembele <yayadembele09@gmail.com>
2023-04-10 17:11:18 +0200

    DAY01

---
+++ DAY01/COURS_LECTURE/FirstCSharpProject/Program.cs
@@ -0,0 +1,12 @@
+﻿// Data Types
+// JavaScript let name = "Alex"
+// Pyton name = "Alex"
+
+
+//-PascalCase: every first letter of every word UpperCase
+
+// String : multiple Characters
+String UserName = "Alex";
+Console.WriteLine(value: UserName);
+// Char : one single character
+// Name = 7;
