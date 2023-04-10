80ba7e10c3077fa71c2efd65ded854075bcbd57d
Yaya Dembele <yayadembele09@gmail.com>
2023-04-10 17:25:36 +0200

    HelloNetCore

---
+++ DAY01/PRACTICE/Hello .NET Core!/HelloNetCore/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! \n");
+Console.WriteLine("I'm Yaya, Web-Developer and SOftware Engineer at Coding Dojo!");
+
