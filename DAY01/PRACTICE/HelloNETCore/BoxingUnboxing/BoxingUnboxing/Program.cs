d65536e9c7e8eb6d7bef8abf746cd224ed8a31f7
Yaya Dembele <yayadembele09@gmail.com>
2023-04-12 04:26:21 +0200

    BoxingUnboxing

---
+++ DAY01/PRACTICE/HelloNETCore/BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,38 @@
+﻿//***************Boxing------Unboxing***************************
+
+//---------------Create an empty List of type object--------------
+
+List<object> myList = new List<object>();
+
+
+//---------------Add the following values to the list: 7, 28, -1, true, "chair"---------------
+
+myList.Add(7);
+myList.Add(28);
+myList.Add(-1);
+myList.Add(true);
+myList.Add("chair");
+
+
+//---------------Loop through the list and print all values-------------------------------
+
+
+for(int i =0; i < myList.Count; i++)
+{
+    Console.WriteLine(myList[i]);
+}
+
+
+//-----------------Add all values that are Int type together and output the sum-----------------------------
+
+
+int somme = 0;
+foreach(object element in myList)
+{
+    
+    if (element is int)
+    {
+        somme += (int)element;
+    }
+}
+Console.WriteLine("La somme des entiers dans la liste est : " + somme);
