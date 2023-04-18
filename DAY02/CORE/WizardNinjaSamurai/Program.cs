03c24ce798c7d47763e24d2837c77f639b17f56d
Yaya Dembele <yayadembele09@gmail.com>
2023-04-18 16:01:47 +0200

    WizardNinjaSamurai.zip

---
+++ DAY02/CORE/WizardNinjaSamurai/Program.cs
@@ -0,0 +1,9 @@
+﻿Human player1 = new Human("John", 10, 5, 8, 100);
+Human player2 = new Human("Jane", 8, 7, 108, 120);
+
+Console.WriteLine($"Before ATTACk: {player1.Name} have {player1.Health}and point of life and {player2.Name} have {player2.Health} and point of life ");
+
+
+player1.Attack(player2);
+
+Console.WriteLine($"After ATTACk: {player1.Name} have {player1.Health}and point of life and {player2.Name} have {player2.Health} and point of life ");
